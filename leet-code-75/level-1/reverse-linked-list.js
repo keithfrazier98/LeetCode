e715ef8e6db48727131edab828d1098b374da187
Keith Frazier <keithers98@gmail.com>
2022-09-30 16:45:55 -0700

    added comments

--- leet-code-75/level-1/reverse-linked-list.js
+++ leet-code-75/level-1/reverse-linked-list.js
@@ -1,11 +1,3 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-
 class ListNode {
   constructor(val, next) {
     this.val = val === undefined ? 0 : val;
